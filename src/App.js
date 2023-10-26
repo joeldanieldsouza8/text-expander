4e02ea11752c199b29859da5e5ad5859c840b26f
Joel Dsouza <joeldsouza8@gmail.com>
2023-10-26 12:38:41 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function App() {
+TextExpander.propTypes = {
+  children: PropTypes.string.isRequired,
+  collapsedNumWords: PropTypes.number,
+  expandButtonText: PropTypes.string,
+  collapseButtonText: PropTypes.string,
+  buttonColor: PropTypes.string,
+  expanded: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
     </div>
   );
 }
 
-export default App;
+/* 
+   The TextExpander component will have the following functionalities:
+    - Display collapsed content by default (can be overridden by an expanded prop).
+    - Show more content when a "Show more" button is clicked.
+    - Show less content when a "Show less" button is clicked.
+*/
+function TextExpander({
+  children, // The text to display.
+  collapsedNumWords = 30, // Default number of words to display is 30.
+  expandButtonText = "Show more", // Default button text is "Show more".
+  collapseButtonText = "Show less", // Default button text is "Show less".
+  buttonColor = "blue", // Default button color is blue.
+  expanded = false, // Default state is collapsed.
+  className = "", // Default class name is an empty string.
+}) {
+  // Define state to keep track of whether the text is expanded or collapsed.
+  // The default state is collapsed.
+  const [isExpanded, setIsExpanded] = useState(expanded);
+
+  // Define a variable to hold the number of words to display.
+  // If the text is expanded, display all the words.
+  // Otherwise, display the first 30 words.
+  const displayedContent = isExpanded
+    ? children
+    : `${children.split(" ").slice(0, collapsedNumWords).join(" ")}...`;
+
+  // Define a function to toggle the expanded state.
+  function toggleExpanded() {
+    setIsExpanded((prevIsExpanded) => !prevIsExpanded);
+  }
+
+  return (
+    <div className={`text-expander ${className}`}>
+      <p>{displayedContent}</p>
+      <button
+        className="text-expander-button"
+        onClick={toggleExpanded}
+        style={{ backgroundColor: buttonColor, outline: "none" }}
+      >
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+}
